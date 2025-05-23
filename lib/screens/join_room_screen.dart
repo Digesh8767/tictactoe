cdc996c69b67221ea0b65001753df33b3249f9f1
Digesh Kapadiya <digesh.belovance@gmail.com>
2025-05-23 08:31:27 +0530

    create room and join room UI created

---
+++ lib/screens/join_room_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/responsive/responsive.dart';
+import 'package:tictactoe/widgets/custom_button.dart';
+import 'package:tictactoe/widgets/custom_texfield.dart';
+import 'package:tictactoe/widgets/custom_text.dart';
+
+class JoinRoomScreen extends StatefulWidget {
+  static String routeName = '/join-room';
+  const JoinRoomScreen({super.key});
+
+  @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _idController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _idController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  )
+                ],
+                text: "Join \nRoom",
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              CustomTexfield(
+                controller: _nameController,
+                hintText: "Enter your nickname",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTexfield(
+                controller: _idController,
+                hintText: "Enter Game ID",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                onTap: () {},
+                text: 'Join',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_texfield.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/utils/colors.dart';
+
+class CustomTexfield extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+
+  const CustomTexfield(
+      {super.key, required this.controller, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.blue,
+            blurRadius: 5,
+            spreadRadius: 2,
+          )
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          fillColor: bgColor,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
